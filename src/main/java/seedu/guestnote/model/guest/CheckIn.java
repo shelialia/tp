dde25116d9d37d799113a9d31ed1f95cc9dcab8f
salads-source <ron52086@gmail.com>
2025-03-13 22:13:26 +0800

    Add check-in and check-out classes

---
+++ src/main/java/seedu/guestnote/model/guest/CheckIn.java
@@ -0,0 +1,10 @@
+package seedu.guestnote.model.guest;
+
+/**
+ * Represents a Check-In date in the guestnote book.
+ */
+public class CheckIn extends StayDuration {
+    public CheckIn(String date) {
+        super(date);
+    }
+}
