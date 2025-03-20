575a3bdcee601197a498b2e8d658fe0d9d574846
aekyr <>
2025-03-20 11:37:03 +0800

    Fix minor checkstyle violations

--- src/main/java/seedu/guestnote/model/request/exceptions/RequestNotFoundException.java
+++ src/main/java/seedu/guestnote/model/request/exceptions/RequestNotFoundException.java
@@ -1,3 +1,6 @@
 package seedu.guestnote.model.request.exceptions;
 
+/**
+ * Signals that the operation is unable to find the specified request.
+ */
 public class RequestNotFoundException extends RuntimeException {}
