c2140f1a01238a46048742e2038090ff10fb2b91
salads-source <ron52086@gmail.com>
2025-03-24 14:48:24 +0800

    Add Ui to display guest status

--- src/main/java/seedu/guestnote/ui/PersonCard.java
+++ src/main/java/seedu/guestnote/ui/PersonCard.java
@@ -37,6 +37,8 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private FlowPane roomNumber;
     @FXML
+    private FlowPane status;
+    @FXML
     private FlowPane requests;
 
 
@@ -51,6 +53,28 @@ public class PersonCard extends UiPart<Region> {
         phone.setText(guest.getPhone().value);
         email.setText(guest.getEmail().value);
         roomNumber.getChildren().add(new Label(guest.getRoomNumber().roomNumber));
+        Label statusLabel = new Label(guest.getStatus().name());
+        statusLabel.getStyleClass().add("status-label");
+
+        switch (guest.getStatus()) {
+
+        case BOOKING:
+            statusLabel.getStyleClass().add("status-booking");
+            break;
+        case CHECKED_IN:
+            statusLabel.getStyleClass().add("status-checkedin");
+            break;
+        case CHECKED_OUT:
+            statusLabel.getStyleClass().add("status-checkedout");
+            break;
+
+        default:
+            break;
+        }
+
+
+        status.getChildren().add(statusLabel);
+
         final int[] counter = {1};
         guest.getRequests()
                 .forEach(request -> {
