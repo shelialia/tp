ace189fcd21e4ad2357f31f64f9e36f084717b4b
salads-source <ron52086@gmail.com>
2025-03-31 11:47:32 +0800

    Refactor check-in and check-out classes to inherit from abstract class

--- src/main/java/seedu/guestnote/logic/commands/CheckInCommand.java
+++ src/main/java/seedu/guestnote/logic/commands/CheckInCommand.java
@@ -1,73 +1,41 @@
 package seedu.guestnote.logic.commands;
 
-import static java.util.Objects.requireNonNull;
-
-import java.util.List;
-
 import seedu.guestnote.commons.core.index.Index;
 import seedu.guestnote.commons.util.ToStringBuilder;
 import seedu.guestnote.logic.Messages;
 import seedu.guestnote.logic.commands.exceptions.CommandException;
 import seedu.guestnote.model.Model;
 import seedu.guestnote.model.guest.Guest;
-import seedu.guestnote.model.guest.Phone;
 import seedu.guestnote.model.guest.Status;
-import seedu.guestnote.model.request.UniqueRequestList;
 
 /**
  * Checks in a guest identified using it's displayed index from the guestnote book.
  */
-public class CheckInCommand extends Command {
+public class CheckInCommand extends StatusChangeCommand {
     public static final String COMMAND_WORD = "check-in";
     public static final String MESSAGE_USAGE = COMMAND_WORD
-            + ": Checks in the guest identified by the index number used in the displayed guest list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+        + ": Checks in the guest identified by the index number used in the displayed guest list.\n"
+        + "Parameters: INDEX (must be a positive integer)\n"
+        + "Example: " + COMMAND_WORD + " 1";
     public static final String MESSAGE_SUCCESS = "Checked in: %1$s";
     public static final String MESSAGE_ALREADY_CHECKED_IN = "Guest is already checked in.";
 
-
-    private final Index targetIndex;
-
-    /**
-     * Constructor to set Index for guest to be checked-out.
-     */
     public CheckInCommand(Index targetIndex) {
-        requireNonNull(targetIndex);
-        this.targetIndex = targetIndex;
+        super(targetIndex);
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        requireNonNull(model);
-        List<Guest> lastShownList = model.getFilteredGuestList();
+        Guest guest = getGuestByIndex(model);
 
-        if (targetIndex.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_GUEST_DISPLAYED_INDEX);
-        }
-
-        Guest guestToCheckIn = lastShownList.get(targetIndex.getZeroBased());
-
-        if (guestToCheckIn.getStatus() == Status.CHECKED_IN) {
+        if (guest.getStatus() == Status.CHECKED_IN) {
             throw new CommandException(MESSAGE_ALREADY_CHECKED_IN);
         }
 
-        UniqueRequestList updatedRequests = new UniqueRequestList();
-        updatedRequests.setRequests(guestToCheckIn.getRequests());
+        Guest updatedGuest = updateGuestStatus(guest, Status.CHECKED_IN);
+        updateModelWithGuest(model, guest, updatedGuest);
 
-        Guest checkedInGuest = new Guest(
-                guestToCheckIn.getName(),
-                guestToCheckIn.getPhone().orElse(new Phone("")),
-                guestToCheckIn.getEmail(),
-                guestToCheckIn.getRoomNumber(),
-                Status.CHECKED_IN,
-                updatedRequests
-        );
-
-        model.setGuest(guestToCheckIn, checkedInGuest);
-        model.updateFilteredGuestList(Model.PREDICATE_SHOW_ALL_GUESTS);
-
-        return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(checkedInGuest)));
+        return new CommandResult(String.format(MESSAGE_SUCCESS, Messages.format(updatedGuest)));
     }
 
     @Override
---
+++ src/main/java/seedu/guestnote/logic/commands/StatusChangeCommand.java
@@ -0,0 +1,54 @@
+package seedu.guestnote.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
+import seedu.guestnote.commons.core.index.Index;
+import seedu.guestnote.logic.Messages;
+import seedu.guestnote.logic.commands.exceptions.CommandException;
+import seedu.guestnote.model.Model;
+import seedu.guestnote.model.guest.Guest;
+import seedu.guestnote.model.guest.Phone;
+import seedu.guestnote.model.guest.Status;
+import seedu.guestnote.model.request.UniqueRequestList;
+
+/**
+ * Abstract command for changing the status of a guest.
+ */
+public abstract class StatusChangeCommand extends Command {
+    protected final Index targetIndex;
+
+    public StatusChangeCommand(Index targetIndex) {
+        this.targetIndex = requireNonNull(targetIndex);
+    }
+
+    protected Guest getGuestByIndex(Model model) throws CommandException {
+        List<Guest> lastShownList = model.getFilteredGuestList();
+
+        if (targetIndex.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_GUEST_DISPLAYED_INDEX);
+        }
+
+        return lastShownList.get(targetIndex.getZeroBased());
+    }
+
+    protected Guest updateGuestStatus(Guest guest, Status newStatus) {
+        UniqueRequestList updatedRequests = new UniqueRequestList();
+        updatedRequests.setRequests(guest.getRequests());
+
+        return new Guest(
+                guest.getName(),
+                guest.getPhone().orElse(new Phone("")),
+                guest.getEmail(),
+                guest.getRoomNumber(),
+                newStatus,
+                updatedRequests
+        );
+    }
+
+    protected void updateModelWithGuest(Model model, Guest oldGuest, Guest newGuest) {
+        model.setGuest(oldGuest, newGuest);
+        model.updateFilteredGuestList(Model.PREDICATE_SHOW_ALL_GUESTS);
+    }
+}
